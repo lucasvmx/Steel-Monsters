fb1e9e60a2ea362960ba6613412b535e08e48fff
Lucas Vieira <lucas.engen@outlook.com>
2021-02-28 02:43:59 -0300

    feat: :sparkles: implemented a basic pause system feat: :sparkles: basic ground and tank drawing feat: :sparkles: basic key events handling
    Signed-off-by: Lucas Vieira <lucas.engen@outlook.com>

--- src/game.c
+++ src/game.c
@@ -1,13 +1,16 @@
 
 #include <raylib.h>
+#include <unistd.h>
 #include "game.h"
 #include "log.h"
+#include "ui.h"
 
 // Predefined functions
 static void DrawFrame();
 
 // Global variables
 static bool gameRunning = false;
+static bool gamePaused = false;
 
 bool isGameRunning()
 {
@@ -18,16 +21,16 @@ void StartGame()
 {
     gameRunning = true;
 
-    println("[INFO] Starting game");
+    log_notice("Starting game");
 
     // Clean screen
     BeginDrawing();
     ClearBackground(RAYWHITE);
-
-    // Run game
-
     // End drawing
     EndDrawing();
+
+    // Run game
+    RunGame();
 }
 
 void RunGame()
@@ -39,9 +42,73 @@ void RunGame()
 
 static void DrawFrame()
 {
+    static int tank_x = 0, tank_y = 100;
+    Rectangle ground, tank;
+    static float elapsed = 0;
+
     ClearBackground(RAYWHITE);
     DrawFPS(0, 0);
 
-    // Read keys
-    
+    if(gamePaused) {
+        DrawText("GAME PAUSED", (GAME_WINDOW_WIDTH / 6) * 2, 
+        (GAME_WINDOW_HEIGHT / 2) - (GAME_WINDOW_HEIGHT / 4), 50, BLUE);
+    }
+
+    elapsed += GetFrameTime();
+
+    if(IsKeyDown(KEY_P) && (elapsed > 0.25)) {
+        gamePaused = !gamePaused;
+
+        if(gamePaused) {
+            log_notice("Game paused: %lf", elapsed);
+        } else {
+            log_notice("Game unpaused");
+        }
+
+        elapsed = 0;
+    }
+
+    const int tank_width = 30, tank_height = 30;
+
+    // Draw earth
+    const int ground_height = GAME_WINDOW_HEIGHT - (GAME_WINDOW_HEIGHT / 3);
+    DrawRectangle(0, ground_height, GAME_WINDOW_WIDTH, ground_height, BROWN);
+
+    ground.height = ground_height;
+    ground.width = GAME_WINDOW_WIDTH;
+    ground.x = 0;
+    ground.y = ground_height;
+
+    // Tank is on ground
+    tank_y = ground.y - tank_height;
+
+    // Draw tank
+    DrawRectangle(tank_x, tank_y, tank_width, tank_height, BuildColor(255, 0, 0));
+
+    tank.x = tank_x;
+    tank.y = tank_y;
+    tank.width = tank_width;
+    tank.height = tank_height;
+
+    if(!gamePaused) {
+        if(IsKeyDown(KEY_D)) {
+            // Move tank to the right
+            if(tank_x < GAME_WINDOW_WIDTH) {
+                tank_x += 1;
+            } else {
+                log_notice("You can't move to the right anymore");
+                tank_x = GAME_WINDOW_WIDTH - (tank_width + 10);
+            }
+        } else if(IsKeyDown(KEY_A)) {
+            // Move to left
+            if(tank_x > 0) {
+                tank_x -= 1;
+            } else {
+                log_notice("You can't move to the left anymore");
+            }
+        } else if(IsKeyDown(KEY_SPACE)) {
+            // Shoot
+
+        }
+    }
 }
