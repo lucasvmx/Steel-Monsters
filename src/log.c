ee83cf52570b02c05714e3b24a70408ae82603c4
Lucas Vieira <lucas.engen@outlook.com>
2021-02-28 02:43:42 -0300

    refactor: :art: improve format of logging functions
    Signed-off-by: Lucas Vieira <lucas.engen@outlook.com>

--- src/log.c
+++ src/log.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include "log.h"
 
+void log_text(const char *prefix, const char *text, va_list list)
+{
+    printf("%s ", prefix);
+    vfprintf(stdout, text, list);
+    fprintf(stdout, "\n");
+}
 
-void println(const char *text, ...)
+void log_notice(const char *text, ...)
 {
     va_list list;
 
     va_start(list, text);
-    vfprintf(stdout, text, list);
-    fprintf(stdout, "\n");
+    log_text("[INFO] ", text, list);
+    va_end(list);
+}
+
+void log_error(const char *text, ...)
+{
+    va_list list;
+
+    va_start(list, text);
+    log_text("[ERROR] ", text, list);
     va_end(list);
 }
--- src/log.h
+++ src/log.h
@@ -1,7 +1,16 @@
-#ifndef LOG_H
-#define LOG_H
+#ifndef LOGGER_H
+#define LOGGER_H
 
 #include <stdarg.h>
+
+/**
+ * @brief 
+ * 
+ * @param prefix 
+ * @param text 
+ * @param ... 
+ */
+extern void log_text(const char *prefix, const char *text, va_list list);
 
 /**
  * @brief Prints a text into stdout
@@ -9,6 +18,13 @@
  * @param text Text to be printed
  * @param ... Arguments list
  */
-extern void println(const char *text, ...);
+extern void log_notice(const char *text, ...);
 
+/**
+ * @brief 
+ * 
+ * @param text 
+ * @param ... 
+ */
+extern void log_error(const char *text, ...);
 #endif
